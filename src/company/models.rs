ecf155ff6bcd6b149e28136b034b9a5a763651c2
bitmargin <bitmargin@hotmail.com>
2021-07-26 01:07:37 +0200

    Implement CRUD about company object

--- src/company/models.rs
+++ src/company/models.rs
@@ -1,58 +1,181 @@
-use actix_web::{Error, HttpRequest, HttpResponse, Responder};
+use actix_web::web;
 use anyhow::Result;
-use arangors::{AqlQuery, Database};
-use futures::future::{ready, Ready};
+use arangors::{
+    document::{
+        options::{InsertOptions, RemoveOptions, UpdateOptions},
+        response::DocumentResponse,
+    },
+    AqlQuery, Collection, Database, Document,
+};
 use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
+use serde_json::{from_str, json, to_string, to_value, Value};
+use std::collections::HashMap;
 use uclient::reqwest::ReqwestClient;
 
 use crate::config::db_database;
 use crate::database::{DbConn, DbPool};
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CompanyResquest {
-    pub name: String,
-    pub since: DateTime<Utc>,
-    pub created_at: DateTime<Utc>,
-    pub modified_at: DateTime<Utc>,
+#[derive(Debug, Deserialize)]
+pub struct FindCompaniesParams {
+    pub search: Option<String>,
+    pub sort_by: Option<String>,
+    pub limit: Option<u32>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct DeleteCompanyParams {
+    pub mode: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Company {
-    pub name: String,
-    pub since: DateTime<Utc>,
-    pub created_at: DateTime<Utc>,
-    pub modified_at: DateTime<Utc>,
+    pub name: Option<String>,
+    pub since: Option<DateTime<Utc>>,
+    pub created_at: Option<DateTime<Utc>>,
+    pub modified_at: Option<DateTime<Utc>>,
+    pub deleted_at: Option<DateTime<Utc>>,
 }
 
-// implementation of Actix Responder for Company struct so we can return Company from action handler
-impl Responder for Company {
-    type Error = Error;
-    type Future = Ready<Result<HttpResponse, Error>>;
-
-    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
-        let body = serde_json::to_string(&self).unwrap();
-        // create response and set content type
-        ready(
-            Ok(
-                HttpResponse::Ok()
-                    .content_type("application/json")
-                    .body(body)
-            )
-        )
+impl Clone for Company {
+    fn clone(&self) -> Company {
+        Company {
+            name: self.name.clone(),
+            since: self.since.clone(),
+            created_at: self.created_at.clone(),
+            modified_at: self.modified_at.clone(),
+            deleted_at: self.deleted_at.clone(),
+        }
     }
 }
 
 // Implementation for Company struct, functions for read/write/update and delete todo from database
 impl Company {
-    pub fn find_all(pool: &DbPool) -> Result<Vec<Company>, &'static str> {
+    pub fn find(params: &web::Query<FindCompaniesParams>, pool: &DbPool) -> Result<Vec<Company>, &'static str> {
         let conn: DbConn = pool.get().unwrap();
         let db: Database<ReqwestClient> = conn.db(&db_database()).unwrap();
+        let mut terms = vec!["FOR c IN companies"];
+        let mut vars: HashMap<&str, Value> = HashMap::new();
+        let search = params.search.as_ref();
+        if search.is_some() {
+            terms.push("FILTER CONTAINS(c.name, @@search)");
+            vars.insert("@search", to_value(search).unwrap());
+        }
+        let sort_by = params.sort_by.as_ref();
+        if sort_by.is_some() {
+            terms.push("SORT c.@@sort_by ASC");
+            vars.insert("@sort_by", to_value(sort_by).unwrap());
+        }
+        let limit = params.limit.as_ref();
+        if limit.is_some() {
+            terms.push("LIMIT 0, @@limit");
+            vars.insert("@limit", to_value(limit).unwrap());
+        }
+        terms.push("RETURN c");
+        let q = terms.join(" ");
         let aql = AqlQuery::builder()
-            .query("FOR c IN @@collection LIMIT 10 RETURN c")
-            .bind_var("@collection", "companies")
+            .query(&q)
+            .bind_vars(vars)
             .build();
-        let results: Vec<Company> = db.aql_query(aql).unwrap();
-        Ok(results)
+        let records: Vec<Company> = db.aql_query(aql).unwrap();
+        Ok(records)
+    }
+
+    pub fn show(key: &web::Path<String>, pool: &DbPool) -> Result<Company, &'static str> {
+        let conn: DbConn = pool.get().unwrap();
+        let db: Database<ReqwestClient> = conn.db(&db_database()).unwrap();
+        let collection: Collection<ReqwestClient> = db.collection("companies").unwrap();
+        let res: Document<Company> = collection.document(key.as_ref()).unwrap();
+        let record: Company = res.document;
+        Ok(record)
+    }
+
+    pub fn create(params: web::Form<Company>, pool: &DbPool) -> Result<Company, &'static str> {
+        let conn: DbConn = pool.get().unwrap();
+        let db: Database<ReqwestClient> = conn.db(&db_database()).unwrap();
+        let collection: Collection<ReqwestClient> = db.collection("companies").unwrap();
+        let now = Utc::now();
+        let data = Company {
+            name: params.name.clone(),
+            since: params.since,
+            created_at: Some(now),
+            modified_at: Some(now),
+            deleted_at: None,
+        };
+        let options: InsertOptions = InsertOptions::builder()
+            .return_new(true)
+            .build();
+        let res: DocumentResponse<Document<Company>> = collection.create_document(Document::new(data), options).unwrap();
+        let record: &Company = res.new_doc().unwrap();
+        Ok(record.clone())
+    }
+
+    pub fn update(key: &web::Path<String>, params: web::Form<Company>, pool: &DbPool) -> Result<Company, &'static str> {
+        let conn: DbConn = pool.get().unwrap();
+        let db: Database<ReqwestClient> = conn.db(&db_database()).unwrap();
+        let collection: Collection<ReqwestClient> = db.collection("companies").unwrap();
+        let obj: Value = json!({
+            "modified_at": Utc::now(),
+        });
+        let text: String = to_string(&obj).unwrap();
+        let mut data: Company = from_str::<Company>(&text).unwrap();
+        if params.name.is_some() {
+            data.name = params.name.clone();
+        }
+        if params.since.is_some() {
+            data.since = params.since.clone();
+        }
+        let options: UpdateOptions = UpdateOptions::builder()
+            .return_new(true)
+            .return_old(true)
+            .build();
+        let res: DocumentResponse<Document<Company>> = collection.update_document(key, Document::new(data), options).unwrap();
+        let record: &Company = res.new_doc().unwrap();
+        Ok(record.clone())
+    }
+
+    pub fn erase(key: &web::Path<String>, pool: &DbPool) -> Result<Company, &'static str> {
+        let conn: DbConn = pool.get().unwrap();
+        let db: Database<ReqwestClient> = conn.db(&db_database()).unwrap();
+        let collection: Collection<ReqwestClient> = db.collection("companies").unwrap();
+        let options: RemoveOptions = RemoveOptions::builder()
+            .return_old(true)
+            .build();
+        let res: DocumentResponse<Document<Company>> = collection.remove_document(key.as_ref(), options, None).unwrap();
+        let record: &Company = res.old_doc().unwrap();
+        Ok(record.clone())
+    }
+
+    pub fn trash(key: &web::Path<String>, pool: &DbPool) -> Result<Company, &'static str> {
+        let conn: DbConn = pool.get().unwrap();
+        let db: Database<ReqwestClient> = conn.db(&db_database()).unwrap();
+        let collection: Collection<ReqwestClient> = db.collection("companies").unwrap();
+        let obj = json!({
+            "deleted_at": Utc::now(),
+        });
+        let text = to_string(&obj).unwrap();
+        let data: Company = from_str::<Company>(&text).unwrap();
+        let options: UpdateOptions = UpdateOptions::builder()
+            .return_new(true)
+            .return_old(true)
+            .build();
+        let res: DocumentResponse<Document<Company>> = collection.update_document(key, Document::new(data), options).unwrap();
+        let record: &Company = res.new_doc().unwrap();
+        Ok(record.clone())
+    }
+
+    pub fn restore(key: &web::Path<String>, pool: &DbPool) -> Result<Company, &'static str> {
+        let conn: DbConn = pool.get().unwrap();
+        let db: Database<ReqwestClient> = conn.db(&db_database()).unwrap();
+        let collection: Collection<ReqwestClient> = db.collection("companies").unwrap();
+        let data: Company = from_str::<Company>("{\"deleted_at\":null}").unwrap();
+        let options: UpdateOptions = UpdateOptions::builder()
+            .return_new(true)
+            .return_old(true)
+            .keep_null(false)
+            .build();
+        let res: DocumentResponse<Document<Company>> = collection.update_document(key, Document::new(data), options).unwrap();
+        let record: &Company = res.new_doc().unwrap();
+        Ok(record.clone())
     }
 }
--- src/company/routes.rs
+++ src/company/routes.rs
@@ -1,22 +1,78 @@
-use actix_web::{delete, get, post, put, web, Error, HttpRequest, HttpResponse};
+use actix_web::{delete, get, post, put, web, Error, HttpResponse};
 
-use crate::company::{Company, CompanyResquest};
+use crate::company::{
+    Company,
+    FindCompaniesParams,
+    DeleteCompanyParams,
+};
 use crate::database::{DbPool};
 
 #[get("/companies")]
-async fn find_all(
-    req: HttpRequest,
+async fn find(
+    params: web::Query<FindCompaniesParams>,
     pool: web::Data<DbPool>,
 ) -> Result<HttpResponse, Error> {
-    let result = web::block(move || Company::find_all(&pool)).await?;
+    let result = web::block(move || Company::find(&params, &pool)).await?;
     Ok(HttpResponse::Ok().json(result))
+}
+
+#[get("/companies/{key}")]
+async fn show(
+    key: web::Path<String>,
+    pool: web::Data<DbPool>,
+) -> Result<HttpResponse, Error> {
+    let result = web::block(move || Company::show(&key, &pool)).await?;
+    Ok(HttpResponse::Ok().json(result))
+}
+
+#[post("/companies")]
+async fn create(
+    params: web::Form<Company>,
+    pool: web::Data<DbPool>,
+) -> Result<HttpResponse, Error> {
+    let result = web::block(move || Company::create(params, &pool)).await?;
+    Ok(HttpResponse::Ok().json(result))
+}
+
+#[put("/companies/{key}")]
+async fn update(
+    key: web::Path<String>,
+    params: web::Form<Company>,
+    pool: web::Data<DbPool>,
+) -> Result<HttpResponse, Error> {
+    let result = web::block(move || Company::update(&key, params, &pool)).await?;
+    Ok(HttpResponse::Ok().json(result))
+}
+
+#[delete("/companies/{key}")]
+async fn delete(
+    key: web::Path<String>,
+    params: web::Form<DeleteCompanyParams>,
+    pool: web::Data<DbPool>,
+) -> Result<HttpResponse, Error> {
+    let mode = params.mode.clone();
+    let result = web::block(move || {
+        if params.mode == "erase" {
+            return Company::erase(&key, &pool);
+        } else if params.mode == "trash" {
+            return Company::trash(&key, &pool);
+        } else if params.mode == "restore" {
+            return Company::restore(&key, &pool);
+        }
+        return Company::erase(&key, &pool);
+    }).await?;
+    if mode == "erase" {
+        return Ok(HttpResponse::NoContent().json({}));
+    } else {
+        return Ok(HttpResponse::Ok().json(result));
+    }
 }
 
 // function that will be called on new Application to configure routes for this module
 pub fn init(cfg: &mut web::ServiceConfig) {
-    cfg.service(find_all);
-    // cfg.service(find);
-    // cfg.service(create);
-    // cfg.service(update);
-    // cfg.service(delete);
+    cfg.service(find);
+    cfg.service(show);
+    cfg.service(create);
+    cfg.service(update);
+    cfg.service(delete);
 }
